5ab8de39cd6bfd08e58b1d6a3eb120e78f01f85c
Pawel Buczek <pawbu@users.noreply.github.com>
2020-12-02 12:15:25 +0100

    Initial commit from spring initializr

---
+++ src/main/java/no/microdata/datastore/MicrodataDatastoreApplication.java
@@ -0,0 +1,13 @@
+package no.microdata.datastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrodataDatastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrodataDatastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/microdata/datastore/MicrodataDatastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package no.microdata.datastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrodataDatastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
