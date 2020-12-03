9675a2ec15027a8d7433e1ffe29aec1bf0bb1fa0
Pawel Buczek <pawbu@users.noreply.github.com>
2020-12-03 08:25:48 +0100

    Import Metadatastore-API classes part 2

--- src/main/java/no/microdata/datastore/services/DataStoreServiceImpl.java
+++ src/main/java/no/microdata/datastore/services/DataStoreServiceImpl.java
@@ -1,9 +1,11 @@
 package no.microdata.datastore.services;
 
 import no.microdata.datastore.DataStoreService;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+@Service
 public class DataStoreServiceImpl implements DataStoreService {
     @Override
     public Map findAllDataStoreVersions(String requestId) {
--- src/main/java/no/microdata/datastore/services/GenericServiceImpl.java
+++ src/main/java/no/microdata/datastore/services/GenericServiceImpl.java
@@ -1,10 +1,12 @@
 package no.microdata.datastore.services;
 
 import no.microdata.datastore.GenericService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class GenericServiceImpl implements GenericService {
     @Override
     public List<Map> findLanguages(String requestId) {
