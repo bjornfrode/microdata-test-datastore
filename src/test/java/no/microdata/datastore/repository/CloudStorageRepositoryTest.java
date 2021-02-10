f57ad8d4614700dedf224415ceeff5d68dd81099
vak <vak@macbookpro.local>
2021-02-10 16:37:37 +0100

    Test showing how to access files from GC bucket

--- src/test/java/no/microdata/datastore/repository/CloudStorageRepositoryTest.java
+++ src/test/java/no/microdata/datastore/repository/CloudStorageRepositoryTest.java
@@ -1,11 +1,15 @@
 package no.microdata.datastore.repository;
 
 import com.google.api.gax.paging.Page;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.*;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class CloudStorageRepositoryTest {
 
@@ -19,8 +23,29 @@ public class CloudStorageRepositoryTest {
         Page<Blob> blobs = bucket.list();
 
         for (Blob blob : blobs.iterateAll()) {
-            System.out.println(blob.getName());
+                System.out.println(blob.getName());
         }
+    }
+
+    @Test
+    public void testContent() throws IOException, SQLException {
+        String bucketName = "test-datastore-bucket-microdata-1";
+        String blobName = "test-datastore/dataset/GRUNNSTFDT_MOTTAK/GRUNNSTFDT_MOTTAK__1_0.db";
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        BlobId blobId = BlobId.of(bucketName, blobName);
+        Blob blob = storage.get(blobId);
+        System.out.println(blob.getSize());
+        System.out.println(blob.getName());
+
+        byte[] content = blob.getContent();
+
+        String file = "/Users/vak/projects/github/microdata-test-datastore/src/test/resources/sqlite.db";
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(content);
+        fos.close();
+
+        DriverManager.getConnection("jdbc:sqlite:" + file);
+
 
     }
 }
